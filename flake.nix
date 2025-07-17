89b6e75557523754e741f502a6bcce4342d94e0d
Lucas Vignoli Reis <lucas.vignoli.reis@gmail.com>
2025-07-17 18:34:58 -0300

    First nix develop run

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
   inputs.fenix.url = "github:nix-community/fenix";
   inputs.organist.url = "github:nickel-lang/organist";
 
