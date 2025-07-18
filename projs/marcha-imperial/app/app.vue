939035626060a10a7532d81cda65c4c3d90c7d19
Lucas Vignoli Reis <lucas.vignoli.reis@gmail.com>
2025-07-18 17:44:23 -0300

    First communication between nuxt and nest

--- projs/marcha-imperial/app/app.vue
+++ projs/marcha-imperial/app/app.vue
@@ -1,7 +1,18 @@
 <template>
   <div>
-    <h1>Hello World</h1>
+    <h1>NestAPI returned:</h1>
+    <p>
+      {{result}}
+    </p>
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
   </div>
 </template>
+
+
+<script setup lang="ts">
+
+const result = await useFetch<{ message: string }>(
+  'http://localhost:3001/'
+)
+</script>
